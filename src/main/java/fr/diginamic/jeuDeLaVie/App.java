f1f80027213871fa16cf8423c80eb0cd39821904
Kévin SEGUINEAU <kevinseguineau+git@gmail.com>
2019-04-23 11:21:32 +0200

    initialisation

---
+++ src/main/java/fr/diginamic/jeuDeLaVie/App.java
@@ -0,0 +1,13 @@
+package fr.diginamic.jeuDeLaVie;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
