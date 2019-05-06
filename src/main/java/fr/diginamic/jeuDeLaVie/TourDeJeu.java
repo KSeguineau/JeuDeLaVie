c78175b991f776542b853bfab0930aaad2e546c5
Kévin SEGUINEAU <kevinseguineau+git@gmail.com>
2019-05-06 17:04:23 +0200

    v1.0

--- src/main/java/fr/diginamic/jeuDeLaVie/TourDeJeu.java
+++ src/main/java/fr/diginamic/jeuDeLaVie/TourDeJeu.java
@@ -2,48 +2,38 @@ package fr.diginamic.jeuDeLaVie;
 
 public class TourDeJeu {
 
-	public static MatriceDeJeu newTour(MatriceDeJeu m){
+	public static MatriceDeJeu nouveauTour(MatriceDeJeu m) {
 		MatriceDeJeu newM = new MatriceDeJeu(MatriceUtils.copy(m.getMatrice()));
-		
+
 		for (int i = 0; i < m.getMatrice().length; i++) {
 			for (int j = 0; j < m.getMatrice().length; j++) {
 				newM.modifierVal(i, j, MatriceUtils.enVie(m.getMatrice(), i, j));
 			}
 		}
-		
+
 		return newM;
-		
+
 	}
-	
+
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 
-		int[][] tmp ={
-				{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,1,1,0,0,0,0,0,0,0},
-				{0,0,0,0,0,1,0,0,1,0,0,0,0,0,0},
-				{0,0,0,0,0,0,1,1,1,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},			
-		};
+		int[][] tmp = { { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, };
 		MatriceDeJeu matrice = new MatriceDeJeu(tmp);
-		
-		while(true){
-		matrice.afficher();
-		matrice = TourDeJeu.newTour(matrice);
-		Thread.sleep(500l);
+
+		while (true) {
+			matrice.afficher();
+			matrice = TourDeJeu.nouveauTour(matrice);
+			Thread.sleep(500l);
 		}
-		
+
 	}
 
-	
 }
